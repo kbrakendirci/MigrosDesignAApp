e63a6d79c327f2a51feb4e74e923fd33d9326672
kbrakendirci <kubrakendirci0@gmail.com>
2022-10-15 18:39:54 +0300

    Page Changed finished

--- app/src/main/java/com/kotlinproject/migrosdesign/MainActivity.kt
+++ app/src/main/java/com/kotlinproject/migrosdesign/MainActivity.kt
@@ -2,10 +2,21 @@ package com.kotlinproject.migrosdesign
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.kotlinproject.migrosdesign.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var tasarim : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        tasarim = ActivityMainBinding.inflate(layoutInflater)
+
+
+        setContentView(tasarim.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        NavigationUI.setupWithNavController( tasarim.bottomNav, navHostFragment.navController)
+
     }
 }
